6a648c56456f094845de94302f9250e5686e107c
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-04-30 20:23:04 +0600

    Задание 8.2.2+

--- Q8/Program.cs
+++ Q8/Program.cs
@@ -9,20 +9,32 @@ namespace Q8
         static void Main(string[] args)
         { 
             GetCatalogs();
+
         }
         static void GetCatalogs()
         {
             try
             {
-                DirectoryInfo dirInfo = new DirectoryInfo("С:\\");
+                DirectoryInfo dirInfo = new DirectoryInfo(@"/" /* Или С:\\ для Windows */ );
                 if (dirInfo.Exists)
                 {
                     Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                    Console.WriteLine($"Название каталога: {dirInfo.Name}");
+                    Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
+                    Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
+                    Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
                 }
+
+                DirectoryInfo newDirectory = new DirectoryInfo(@"/newDirectory");
+                if (!newDirectory.Exists)
+                    newDirectory.Create();
+
+                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
+
             }
         }
     }
